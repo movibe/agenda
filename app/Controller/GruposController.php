9d2d2a9f3e8929dd9774b2b3d7732c5d40ddfeda
Willian Ribeiro Angelo <movibe@icloud.com>
2014-01-05 00:52:30 -0200

    Grupos

--- app/Controller/GruposController.php
+++ app/Controller/GruposController.php
@@ -1,5 +1,5 @@
 <?php
-App::uses('AppController', 'Controller');
+App::uses( 'AppController', 'Controller' );
 /**
  * Grupos Controller
  *
@@ -8,105 +8,105 @@ App::uses('AppController', 'Controller');
  */
 class GruposController extends AppController {
 
-/**
- * Components
- *
- * @var array
- */
-	public $components = array('Paginator');
+	/**
+	 * Components
+	 *
+	 * @var array
+	 */
+	public $components = array( 'Paginator' );
 
-/**
- * index method
- *
- * @return void
- */
-	
-	public function getGrupos(){
-		$s['grupos'] = $this->Grupo->generateTreeList(null,null, null, '--');
-		$this->set($s);
+	/**
+	 * index method
+	 *
+	 * @return void
+	 */
+
+	public function getGrupos() {
+		$s['grupos'] = $this->Grupo->generateTreeList( null, null, null, '--' );
+		$this->set( $s );
 	}
 	public function index() {
 		$this->Grupo->recursive = 0;
-		$this->set('grupos', $this->Paginator->paginate());
+		$this->set( 'grupos', $this->Paginator->paginate() );
 
 		self::getGrupos();
 	}
 
-/**
- * view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		if (!$this->Grupo->exists($id)) {
-			throw new NotFoundException(__('Invalid grupo'));
+	/**
+	 * view method
+	 *
+	 * @throws NotFoundException
+	 * @param string  $id
+	 * @return void
+	 */
+	public function view( $id = null ) {
+		if ( !$this->Grupo->exists( $id ) ) {
+			throw new NotFoundException( __( 'Invalid grupo' ) );
 		}
-		$options = array('conditions' => array('Grupo.' . $this->Grupo->primaryKey => $id));
-		$this->set('grupo', $this->Grupo->find('first', $options));
+		$options = array( 'conditions' => array( 'Grupo.' . $this->Grupo->primaryKey => $id ) );
+		$this->set( 'grupo', $this->Grupo->find( 'first', $options ) );
 	}
 
-/**
- * add method
- *
- * @return void
- */
+	/**
+	 * add method
+	 *
+	 * @return void
+	 */
 	public function add() {
-		if ($this->request->is('post')) {
+		if ( $this->request->is( 'post' ) ) {
 			$this->Grupo->create();
-			if ($this->Grupo->save($this->request->data)) {
-				$this->Session->setFlash(__('The grupo has been saved.'), 'default', array('class' => 'alert alert-success'));
-				return $this->redirect(array('action' => 'index'));
+			if ( $this->Grupo->save( $this->request->data ) ) {
+				$this->Session->setFlash( __( 'The grupo has been saved.' ), 'default', array( 'class' => 'alert alert-success' ) );
+				return $this->redirect( array( 'action' => 'index' ) );
 			} else {
-				$this->Session->setFlash(__('The grupo could not be saved. Please, try again.'), 'default', array('class' => 'alert alert-error'));
+				$this->Session->setFlash( __( 'The grupo could not be saved. Please, try again.' ), 'default', array( 'class' => 'alert alert-error' ) );
 			}
 		}
 		self::getGrupos();
 	}
 
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		if (!$this->Grupo->exists($id)) {
-			throw new NotFoundException(__('Invalid grupo'));
+	/**
+	 * edit method
+	 *
+	 * @throws NotFoundException
+	 * @param string  $id
+	 * @return void
+	 */
+	public function edit( $id = null ) {
+		if ( !$this->Grupo->exists( $id ) ) {
+			throw new NotFoundException( __( 'Invalid grupo' ) );
 		}
-		if ($this->request->is(array('post', 'put'))) {
-			if ($this->Grupo->save($this->request->data)) {
-				$this->Session->setFlash(__('The grupo has been saved.'), 'default', array('class' => 'alert alert-success'));
-				return $this->redirect(array('action' => 'index'));
+		if ( $this->request->is( array( 'post', 'put' ) ) ) {
+			if ( $this->Grupo->save( $this->request->data ) ) {
+				$this->Session->setFlash( __( 'The grupo has been saved.' ), 'default', array( 'class' => 'alert alert-success' ) );
+				return $this->redirect( array( 'action' => 'index' ) );
 			} else {
-				$this->Session->setFlash(__('The grupo could not be saved. Please, try again.'), 'default', array('class' => 'alert alert-error'));
+				$this->Session->setFlash( __( 'The grupo could not be saved. Please, try again.' ), 'default', array( 'class' => 'alert alert-error' ) );
 			}
 		} else {
-			$options = array('conditions' => array('Grupo.' . $this->Grupo->primaryKey => $id));
-			$this->request->data = $this->Grupo->find('first', $options);
+			$options = array( 'conditions' => array( 'Grupo.' . $this->Grupo->primaryKey => $id ) );
+			$this->request->data = $this->Grupo->find( 'first', $options );
 		}
 		self::getGrupos();
 	}
 
-/**
- * delete method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
+	/**
+	 * delete method
+	 *
+	 * @throws NotFoundException
+	 * @param string  $id
+	 * @return void
+	 */
+	public function delete( $id = null ) {
 		$this->Grupo->id = $id;
-		if (!$this->Grupo->exists()) {
-			throw new NotFoundException(__('Invalid grupo'));
+		if ( !$this->Grupo->exists() ) {
+			throw new NotFoundException( __( 'Invalid grupo' ) );
 		}
-		$this->request->onlyAllow('post', 'delete');
-		if ($this->Grupo->delete()) {
-			$this->Session->setFlash(__('The grupo has been deleted.'), 'default', array('class' => 'alert alert-success'));
+		$this->request->onlyAllow( 'post', 'delete' );
+		if ( $this->Grupo->delete() ) {
+			$this->Session->setFlash( __( 'The grupo has been deleted.' ), 'default', array( 'class' => 'alert alert-success' ) );
 		} else {
-			$this->Session->setFlash(__('The grupo could not be deleted. Please, try again.'), 'default', array('class' => 'alert alert-error'));
+			$this->Session->setFlash( __( 'The grupo could not be deleted. Please, try again.' ), 'default', array( 'class' => 'alert alert-error' ) );
 		}
-		return $this->redirect(array('action' => 'index'));
+		return $this->redirect( array( 'action' => 'index' ) );
 	}}
